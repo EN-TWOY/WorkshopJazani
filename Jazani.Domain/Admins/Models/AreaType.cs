7ce56a7723f0ce42df2a31ebbeb4f3b367995355
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-02-09 22:10:17 -0500

    feat(Domain): add AreaType

--- Jazani.Domain/Admins/Models/AreaType.cs
+++ Jazani.Domain/Admins/Models/AreaType.cs
@@ -3,9 +3,11 @@ namespace Jazani.Domain.Admins.Models
 {
 	public class AreaType
 	{
-		public AreaType()
-		{
-		}
+		public int Id { get; set; }
+		public string Name { get; set; } = default!;
+		public string? Description { get; set; }
+		public DateTime RegistrationDate { get; set; }
+		public bool State { get; set; }
 	}
 }
 
--- Jazani.Domain/Admins/Repositories/IAreaTypeRepository.cs
+++ Jazani.Domain/Admins/Repositories/IAreaTypeRepository.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using Jazani.Domain.Admins.Models;
+
 namespace Jazani.Domain.Admins.Repositories
 {
 	public interface IAreaTypeRepository
 	{
+		Task<IReadOnlyList<AreaType>> FindAllAsync();
+		Task<AreaType?> FindByIdAsync(int id);
+		Task<AreaType> SaveAsync(AreaType areaType);
 	}
 }
 
